592e25fb7599f9ddcaa9342d4de5a2f52b832e4d
Dilawar Singh <dilawars@ncbs.res.in>
2015-12-08 17:19:53 +0530

    For testing.

--- python/eyeBlinkDataSave.py
+++ python/eyeBlinkDataSave.py
@@ -15,15 +15,54 @@ import datetime
 import csv
 import numpy as np
 import codecs
+import matplotlib
+from multiprocessing  import Process, Queue
+
+q_ = Queue()
+
+try:
+    matplotlib.use('GTKAgg') 
+except Exception as e:
+    print("Cant load GTKAgg. Using default")
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+    format='%(asctime)s -- %(message)s',
+    datefmt='%m-%d %H:%M',
+    filename='blink.log',
+    filemode='w')
+_logger = logging.getLogger('')
+
+
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-# Two threaded queue. One thread writes to queue, other consume it to plot it.
-# globals.
-fig_ = plt.figure()
-ax_ = plt.axes(xlim=(0,1000), ylim=(0, 1000))
+mouse_ = None
+serial_ = None
+
+fig_ = plt.figure( )
+fig_.title = 'Overall profile'
+
+global_ = plt.subplot(2,1,1)
+local_ = plt.subplot(2,1,2)
+
+gax_ = plt.subplot(2, 1, 1)
+gax_.set_xlim([0, 1000])
+gax_.set_ylim([0, 1000])
+gax_.axes.get_xaxis().set_visible(False)
+
+lax_ = plt.subplot(2, 1, 2)
+lax_.set_xlim([0, 1000])
+lax_.set_ylim([0, 1000])
+text_ = gax_.text(0.02, 0.95, '', transform=gax_.transAxes)
+
 # ax.set_autoscalex_on(True)
-line_, = ax_.plot([], [], '*')
+lline_, = lax_.plot([], [], lw=0.5)
+gline_, = gax_.plot([], [])
+gline1_, = gax_.plot(0, 0, 'r*')
+
+ybuff_, xbuff_ = [], []
+stary_, startx_ = [], []
 
 save_dir_ = os.path.join( 
         os.environ['HOME']
@@ -44,7 +83,7 @@ PROFILING_DATA_MARKER = '$'
 SESSION_BEGIN_MARKER = '<'
 SESSION_END_MARKER = '>'
 
-serialPort = None
+serial_port_ = None
 
 def saveDict2csv(filename, fieldnames=[], dictionary={}):
     with open (filename, 'a') as csvfile:
@@ -57,33 +96,34 @@ def saveDict2csv(filename, fieldnames=[], dictionary={}):
 # be read completely from serial-port.
 def getSerialPort(portPath = '/dev/ttyACM'+ str(sys.argv[4]), baudRate = 9600):
     global logWin_
-    global serialPort
-    serialPort = serial.Serial(portPath, baudRate)
-    print('[INFO] Connected to %s' % serialPort)
-    return serialPort
+    global serial_port_
+    global serial_
+    serial_port_ = serial.Serial(portPath, baudRate)
+    print('[INFO] Connected to %s' % serial_port_)
+    serial_ = '%s' % portPath.split('/')[-1]
+    return serial_port_
 
-def writeTrialData(serialPort, saveDirec, trialsDict = {}, arduinoData =[]):
+def writeTrialData(serial_port_, saveDirec, trialsDict = {}, arduinoData =[]):
     #The first line after '@' will give us
-    runningTrial, csType = getLine(serialPort).split()
+    runningTrial, csType = getLine(serial_port_).split()
     trialsDict[runningTrial] = []
     print('[INFO] Trial: %s' %runningTrial)
     
     #Then, wait indefinitely for the DATA_BEGIN_MARKER from the next line
     while True:
-        line = getLine( serialPort )
+        line = getLine( serial_port_ )
         if DATA_BEGIN_MARKER in line:
             break
 
     #Once the DATA_BEGIN_MARKER is caught,
     while True:
-        line = getLine(serialPort)
+        line = getLine(serial_port_)
         if DATA_END_MARKER in line:
             break
         elif not line:
             pass
         else:
             blinkValue, timeStamp = line.split()
-            # print( "Data: %s" % line)
             trialsDict[runningTrial].append((blinkValue, timeStamp))
 
     with open(os.path.join(saveDirec, "Trial" + runningTrial + ".csv"), 'w') as f:
@@ -99,15 +139,15 @@ def writeTrialData(serialPort, saveDirec, trialsDict = {}, arduinoData =[]):
 #     fields = ['BlinkValue', 'TimeStamp']
 #     saveDict2csv(filename, fields, trialsDict)
 
-def writeProfilingData(serialPort, saveDirec, profilingDict = {}, arduinoData = []):
+def writeProfilingData(serial_port_, saveDirec, profilingDict = {}, arduinoData = []):
     
     #Wait indefinitely for the DATA_BEGIN_MARKER
-    while DATA_BEGIN_MARKER not in getLine(serialPort):
+    while DATA_BEGIN_MARKER not in getLine(serial_port_):
         continue
     
     #Once the DATA_BEGIN_MARKER is caught,
     while True:
-        line = getLine(serialPort)
+        line = getLine(serial_port_)
         if DATA_END_MARKER in line:
             break
         elif not line:
@@ -125,9 +165,9 @@ def writeProfilingData(serialPort, saveDirec, profilingDict = {}, arduinoData = 
             f.write("\n".join(data))
 
 def getLine(port = None):
-    global serialPort
+    global serial_port_
     if not port:
-        port = serialPort
+        port = serial_port_
     line = []
     txtLine = u''
     while True:
@@ -136,7 +176,7 @@ def getLine(port = None):
             if c == '\r' or c == '\n' or c == '\r\n':
                 for x in line:
                     try:
-                        txtLine += x.decode('ascii')
+                        txtLine += x
                     except:
                         return ""
                 break
@@ -144,19 +184,18 @@ def getLine(port = None):
                 line.append(c)
     return txtLine
 
-def dump_to_console(serialPort, saveDirec, trialsDict, profilingDict):
-    line = getLine( serialPort )
+def dump_to_console(serial_port_, saveDirec, trialsDict, profilingDict):
+    line = getLine( serial_port_ )
     while SESSION_BEGIN_MARKER not in line:
-        line = getLine( serialPort )
+        line = getLine( serial_port_ )
         line = line.strip()
         if line:
             y, x = line_to_yx( line )
-            line_.set_data(y, x)
-            fig_.canvas.draw()
+            lline_.set_data(y, x)
 
     #Once the SESSION_BEGIN_MARKER is caught,
     while True:
-        arduinoData = getLine(serialPort)
+        arduinoData = getLine(serial_port_)
         if arduinoData:
             q_.put(arduinoData)
         if SESSION_END_MARKER in arduinoData:
@@ -166,22 +205,24 @@ def dump_to_console(serialPort, saveDirec, trialsDict, profilingDict):
         elif COMMENT_MARKER in arduinoData:
             pass
         elif TRIAL_DATA_MARKER in arduinoData:
-            writeTrialData(serialPort, saveDirec)
+            writeTrialData(serial_port_, saveDirec)
         elif PROFILING_DATA_MARKER in arduinoData:
-            writeProfilingData(serialPort, saveDirec)
+            writeProfilingData(serial_port_, saveDirec)
         else:
             print("[WARNING] No instructions for %s defined in dump_to_console()" %arduinoData)
 
 def start():
-    global serialPort
+    global serial_port_
     global save_dir_
-    serialPort = getSerialPort()
-    serialPort.write(sys.argv[1])
+    global serial_, mouse_
+    serial_port_ = getSerialPort()
+    serial_port_.write(sys.argv[1])
     time.sleep(1)
-    serialPort.write(sys.argv[2])
+    serial_port_.write(sys.argv[2])
     time.sleep(1)
-    serialPort.write(sys.argv[3])
+    serial_port_.write(sys.argv[3])
     timeStamp = datetime.datetime.now().isoformat()
+    mouse_ = sys.argv[1]
     if len(sys.argv) <= 1:
         outfile = os.path.join( timeStamp , 'raw_data')
     else:
@@ -198,27 +239,16 @@ def start():
 def produce_data():
     # Here we dump the data onto console/cursed console.
     global save_dir_
-    profilingDict = {}
-    trialsDict = defaultdict(list)
-    dump_to_console(serialPort, save_dir_, trialsDict, profilingDict)
-    serialPort.close()
-
-def get_lines_for_half_second( serialPort ):
-    startT = time.time()
-    xdata, ydata = [], []
-    while time.time() - startT < 0.5:
-        l = getLine( serialPort )
-        if not l.strip():
-            continue
-        l = l.split(' ')
-        if not l:
-            continue
-        if len(l) == 1: 
-            l.append( len(xdata) + 2 )
-        y, x = l
-        xdata.append(float(x))
-        ydata.append(float(y))
-    return (xdata, ydata)
+    global q_
+    while True:
+        line = getLine( serial_port_ )
+        y, x = line_to_yx( line )
+        if x and y:
+            try:
+                x, y = float(x), int(y)
+                q_.put((y,x))
+            except Exception as e:
+                _logger.info("Could not convert: %s, %s" % (y, x))
 
 def line_to_yx( line ):
     if not line.strip():
@@ -228,58 +258,76 @@ def line_to_yx( line ):
         return (None, None)
     if len(l) == 1: 
         l.append( time.time() - tstart )
+    try:
+        return (float(l[0]), float(l[1]))
+    except Exception as e:
+        return (None, None)
     return l
 
 def get_data():
-    global q_, line_
+    global q_, lline_
     while True:
         item =  q_.get()
         y, x = line_to_yx( item )
         if x:
-            print(y, x)
-            line_.set_data(y, x)
+            lline_.set_data(y, x)
             fig_.canvas.draw()
 
+def sanitize_data( xvec, yvec):
+    data = zip(xvec, yvec)
+    return zip(*data)
+
 def init():
-    line_.set_data([], [])
-    return line_,
+    lline_.set_data([], [])
+    text_.set_text('')
+    return lline_, gline_, gline1_, text_
 
 def animate(i):
-    global line_
-    line = getLine()
-    y, x = line_to_yx(line)
-    if x and i % 100 == 0:
-        xmax = ax_.get_xlim()
-        if float(x) + 1 > xmax:
-            ax_.set_xlim((xmax-500, xmax+500))
-        line_.set_data( float(x)+1, y )
-    return line_,
+    global lline_
+    global serial_, mouse_
+    global xbuff_, ybuff_
+    global q_
+
+    # Get 20 elements from queue and plot them.
+    for i in range(20):
+        y, x = q_.get()
+        ybuff_.append(y)
+        xbuff_.append( len(ybuff_) + 1 )
+    _logger.info("Got from queue: %s, %s" % (xbuff_[-20:], ybuff_[-20:]))
+
+    xmin, xmax = lax_.get_xlim()
+    if len(xbuff_) >= xmax:
+        print("+ Updating axes")
+        lax_.set_xlim((xmax-1000, xmax+1000))
+        gax_.set_xlim((xmax-3000, xmax))
+        xbuff_ = xbuff_[-3000:]
+        ybuff_ = ybuff_[-3000:]
+        gline_.set_data(xbuff_, ybuff_)
+        startx_.append(xbuff_[-1])
+        stary_.append(50)
+        gline1_.set_data(startx_, stary_)
+    # data = sanitize_data(xbuff_, ybuff_)
+    # lline_.set_data(data[0][-1000:], data[1][-1000:])
+    lline_.set_data(xbuff_[-1000:], ybuff_[-1000:])
+    text = 'TIME: %.3f' % (time.time() - tstart)
+    text += ' MOUSE: %s' % mouse_
+    text += ' SERIAL: %s' % serial_
+    text_.set_text(text)
+    return lline_, gline_, gline1_, text_
 
 def main():
     start()
+    p = Process( target = produce_data )
+    p.start()
     anim = animation.FuncAnimation(fig_
             , animate
             , init_func = init
-            , frames=20
+            , frames=1
             , interval = 1
             , blit = True
             )
     plt.show()
-
-
-def test():
-    filename = None
-    for d, ds, fs in os.walk(os.environ['HOME']+"/Desktop/Work/Behaviour/"): 
-        for f in fs:
-            if "Trial1.csv" in f:
-                filename = os.path.join(d, f)
-                break
-    with open(filename, "r") as f:
-        lines = f.read().split('\n')
-    data = lines[3:]
-    initCurses()
-    for d in data:
-        d = d.replace(',',  ' ')
+    p.join()
 
 if __name__ == '__main__':
     main()
