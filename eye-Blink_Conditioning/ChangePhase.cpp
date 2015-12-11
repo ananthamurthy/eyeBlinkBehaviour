60ca8c9feef55fddc838bf25ba2e0cfcb70c651b
Behaviour Room V4 <dilawars@ncbs.res.in>
2015-12-11 15:21:28 +0530

    Added accidently removed folder.

---
+++ eye-Blink_Conditioning/ChangePhase.cpp
@@ -0,0 +1,62 @@
+//
+// ChangePhase.cpp 
+// C++ code
+// ----------------------------------
+// Developed with embedXcode 
+// http://embedXcode.weebly.com
+//
+// Project 		eye-Blink_Conditioning
+//
+// Created by 	Kambadur Ananthamurthy, 13/09/15 11:15 am
+// 				Kambadur Ananthamurthy
+//
+// Copyright	© Kambadur Ananthamurthy, 2015
+// Licence   	<#license#>
+//
+// See 			ChangePhase.h and ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+#if defined(WIRING) // Wiring specific
+#   include "Wiring.h"
+#elif defined(MAPLE_IDE) // Maple specific
+#   include "WProgram.h"
+#elif defined(MPIDE) // chipKIT specific
+#   include "WProgram.h"
+#elif defined(DIGISPARK) // Digispark specific
+#   include "Arduino.h"
+#elif defined(ENERGIA) // LaunchPad specific
+#   include "Energia.h"
+#elif defined(LITTLEROBOTFRIENDS) // LittleRobotFriends specific
+#   include "LRF.h"
+#elif defined(MICRODUINO) // Microduino specific
+#   include "Arduino.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#   include "Arduino.h"
+#elif defined(REDBEARLAB) // RedBearLab specific
+#   include "Arduino.h"
+#elif defined(SPARK) // Spark specific
+#   include "application.h"
+#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
+#   include "Arduino.h"
+#else // error
+#   error Platform not defined
+#endif // end IDE
+
+// Code
+#include "Globals.h"
+#include "LCDRelated.h"
+#include "ChangePhase.h"
+
+extern int condition;
+extern int trialNum;
+extern unsigned long startPhaseTime;
+
+void changePhase( int cond, int status )
+{
+    condition = cond;
+    printStatus(status, trialNum);
+    lastTime = 0;
+    startPhaseTime = millis();
+}
