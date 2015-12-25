e280e0d3cdf91bec360dc08c79d78c181b618826
Dilawar Singh <dilawars@ncbs.res.in>
2015-12-25 14:22:51 +0530

    SHell script to autmate the process.

---
+++ analysis/run_on_all_session.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+
+if [ $# -lt 1 ]; then
+    echo "USAGE: $0 data_dir"
+    exit
+fi
+
+RESDIR=_results
+mkdir -p $RESDIR
+DIRS=`find $1 -type d -name "*SessionType*Session*"`
+for d in $DIRS; do
+    echo "Processing $d"
+    python ./analyze_data.py -d $d 
+    find $d -type f -name "*.png" -or -name "*.svg" | xargs -I file cp file $RESDIR
+done
+echo "All results files are also copied to $RESDIR"
