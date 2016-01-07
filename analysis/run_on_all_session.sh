7ed68f32d37e282b18ecbf920e091c4fd673ad26
Dilawar Singh <dilawars@ncbs.res.in>
2016-01-07 14:12:44 +0530

    Now more copying.

--- analysis/run_on_all_session.sh
+++ analysis/run_on_all_session.sh
@@ -5,13 +5,10 @@ if [ $# -lt 1 ]; then
     exit
 fi
 
-RESDIR=_results
 mkdir -p $RESDIR
 DIRS=`find $1 -type d -name "*SessionType*Session*"`
 for d in $DIRS; do
     echo "Processing $d"
-    python ./analyze_data.py -d $d  -o `pwd`/_results/`basename $d`
-    find $d -type f -name "*.png" -or -name "*.svg" | xargs -I file cp file $RESDIR
+    python ./analyze_data.py -d $d  -o `pwd`/_results/`basename $d` | tee ${d}/log.log
 done
-echo "All results files are also copied to $RESDIR"
 
