ae388b63898e51de211d02ed1cd9ae6f70e13856
Behaviour Room V4 <dilawars@ncbs.res.in>
2016-02-05 18:10:51 +0530

    code clean-up to identify odd/even sessionType_ind

--- src/eye_blink_main.ino
+++ src/eye_blink_main.ino
@@ -201,7 +201,8 @@ void loop()
                 if (currentPhaseTime >= preTime)
                 {
                     // If sessionType_ind involves LED
-                    if ((sessionType_ind == 1) || (sessionType_ind == 3) || (sessionType_ind == 5) || (sessionType_ind == 7) || (sessionType_ind == 9) || (sessionType_ind == 11))
+                    //if ((sessionType_ind == 1) || (sessionType_ind == 3) || (sessionType_ind == 5) || (sessionType_ind == 7) || (sessionType_ind == 9) || (sessionType_ind == 11))
+                    if (sessionType_ind % 2)
                     {
                         if( true == CS_plus )
                         {
