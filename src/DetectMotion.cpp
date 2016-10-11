4e1378add7ff6ba5191d254e0c9a63ddbd60daf6
ananthamurthy <kganantha@gmail.com>
2016-10-11 18:26:24 +0530

    added missing source files

---
+++ src/DetectMotion.cpp
@@ -0,0 +1,31 @@
+//
+// DetectMotion.cpp
+// C++ code
+// ----------------------------------
+// Developed with embedXcode
+// http://embedXcode.weebly.com
+//
+// Project 		eye-Blink_Conditioning
+//
+// Created by 	Kambadur Ananthamurthy, 04/08/15 1:45 pm
+// 				Kambadur Ananthamurthy
+//
+// Copyright	© Kambadur Ananthamurthy, 2015
+// Licence   	<#license#>
+//
+// See 			DetectBlinks.h and ReadMe.txt for references
+//
+
+// Code
+#include "Globals.h"
+#include "DetectMotion.h"
+
+void detectMotion() //(unsigned long currentTime)
+{
+    if ( currentPhaseTime - lastTime > sampleInterval )
+	{        
+		motion = digitalRead(motion_di);
+        write_data_line( motion, currentPhaseTime % 10000 );
+        lastTime += sampleInterval;
+    }
+}
