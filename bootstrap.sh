e7e0bc0c3d2855298abff38fb3bb1f781782341f
Dilawar Singh <dilawar.s.rajput@gmail.com>
2017-01-03 18:49:19 +0530

    Added bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set +x 
+set -e
 
 sudo apt install -y  libopencv-*
 sudo apt install -y  libboost-dev 
@@ -17,4 +19,11 @@ if [ ! -f /etc/udev/rules.d/40-pgr.rules ]; then
     echo "Configuring for camera"
     bash ./PointGreyCamera/external/spin-conf
 fi
+
+sudo mkdir -p /mnt/ramdisk
+
 echo "All done. You may like to reboot/logout"
+echo "You may like mount a slice of ram to /mnt/ramdisk"
+echo "Something like the following needs to be added to /etc/fstab"
+printf "\ttmpfs /mnt/ramdisk tmpfs nosuid,size=1024M 0 0\n"
+printf "To make it permanent\n"
