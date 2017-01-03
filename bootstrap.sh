66632ac1cc567cb4f1b62e9910b97a094f30b9c5
Dilawar Singh <dilawar.s.rajput@gmail.com>
2017-01-03 18:32:35 +0530

    Added bootstrap script.

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo apt install -y  libopencv-*
+sudo apt install -y  libboost-dev 
+sudo apt install -y  cmake
+sudo apt install -y  python-numpy 
+sudo apt install -y  python-opencv 
+sudo apt install -y  arduino
+sudo apt install -y  python-pip
+sudo apt install -y  gnuplot 
+sudo apt install -y  python-tifffile python-pillow  python-matplotlib
+sudo -E pip install gnuplotlib readchar  
+
+
