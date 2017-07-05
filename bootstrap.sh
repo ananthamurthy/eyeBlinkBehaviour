04cb85698ef5eae0b14ab6d49e3fdcb35ed23e0c
Dilawar Singh <dilawars@ncbs.res.in>
2017-07-05 10:14:39 +0530

    One apt command to rule them all.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,15 @@
 set +x 
 set -e
 
-sudo apt install -y  libopencv-*
-sudo apt install -y  libboost-dev 
-sudo apt install -y  cmake
-sudo apt install -y  python-numpy 
-sudo apt install -y  python-opencv 
-sudo apt install -y  arduino
-sudo apt install -y  python-pip
-sudo apt install -y  gnuplot 
-sudo apt install -y  python-tifffile python-pillow  python-matplotlib
+sudo apt install -y  libopencv-* \
+  libboost-dev  \
+  cmake \
+  python-numpy \
+  python-opencv \ 
+  arduino \
+  python-pip \
+  gnuplot \
+  python-tifffile python-pillow  python-matplotlib \
 sudo -E pip install gnuplotlib readchar  
 
 echo "Now adding user to all the cool groups"
